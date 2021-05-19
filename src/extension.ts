714cd7ad95c7c6f14a3ade08b8c1dc2ad38b8e82
YouAreNotDefined <for.1self.ik@gmail.com>
2021-05-19 14:37:40 +0000

    ファイル探す

--- src/extension.ts
+++ src/extension.ts
@@ -8,13 +8,17 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let disposable = vscode.commands.registerCommand('encodeISO2022JP.command', () => {
 		const editor = vscode.window.activeTextEditor;
+		if (typeof editor === 'undefined') {
+			vscode.window.showInformationMessage('No active window!');
+			return;
+		}
 		const model = new Model(editor);
 		if (model.charCode === 'ISO-2022-JP') {
 			StatusBar.encoding();
 			model.encodeFile();
 			StatusBar.notEncoding();
 		} else {
-			vscode.window.showInformationMessage('Can only convert to ISO-2022-JP');
+			vscode.window.showInformationMessage('Can only convert the ISO-2022-JP');
 		}
 	});
 
--- src/model.ts
+++ src/model.ts
@@ -2,34 +2,26 @@ import * as vscode from 'vscode';
 import * as encoding from "encoding-japanese";
 import * as Jschardet from "jschardet";
 import * as Iconv from "iconv-lite";
-
-// import { TextEncoder } from 'util';
+import * as fs from "fs";
 
 export class Model {
-  private static editor: vscode.TextEditor | undefined;
+  private static editor: vscode.TextEditor;
 
-  constructor(editor: vscode.TextEditor | undefined) {
+  constructor(editor: vscode.TextEditor) {
     Model.editor = editor;
   }
 
   private static get fileDoc() {
-    if (typeof Model.editor === undefined) {
-      vscode.window.showInformationMessage('No active window!');
-      return;
-    }
-
     return Model.editor?.document.getText();
   }
 
   private static get convertToUTF8() {
-    assertIsDefined(Model.fileDoc);
     const buf = Buffer.from(Model.fileDoc, 'binary');
     const docUtf8 = Iconv.decode(buf, 'utf-8');
     return docUtf8;
   }
 
   private static get unicodeArray() {
-    assertIsDefined(Model.fileDoc);
     const unicodeArray: number[] = [];
     for (let i = 0; i < Model.fileDoc.length; i++) {
       unicodeArray.push(Model.fileDoc.charCodeAt(i));
@@ -37,22 +29,22 @@ export class Model {
     return unicodeArray;
   }
 
+  private static get readFile() {
+    return fs.readFileSync(`${Model.editor?.document.uri.fsPath}`);
+  }
+
   get charCode() {
-    assertIsDefined(Model.fileDoc);
-    const charCode = Jschardet.detect(Model.fileDoc);
-    return charCode.encoding;
+    return Jschardet.detect(Model.fileDoc).encoding;
   }
 
   encodeFile() {
-    assertIsDefined(Model.fileDoc);
-    assertIsDefined(Model.editor);
-
-    const JISArray = encoding.convert(Model.unicodeArray, {
+    const Utf8Array = encoding.convert(Model.unicodeArray, {
       to: 'UTF8',
       from: 'AUTO'
     });
-    const text = encoding.codeToString(JISArray);
-    // vscode.window.showInformationMessage(Jschardet.detect(encoding.codeToString(JISArray)).encoding)
+    const text = encoding.codeToString(Utf8Array);
+
+    vscode.window.showInformationMessage(`${Model.readFile}`)
 
     const startPos = new vscode.Position(0, 0);
     const endPos = new vscode.Position(Model.editor?.document.lineCount - 1, 10000);
