7865712375d710dcf3ba80924a63ec81088fd608
YouAreNotDefined <for.1self.ik@gmail.com>
2021-05-22 11:55:56 +0000

    iconvとwriteFileの導入

--- src/model.ts
+++ src/model.ts
@@ -1,11 +1,13 @@
 import * as vscode from 'vscode';
 import * as encoding from "encoding-japanese";
 import * as Jschardet from "jschardet";
-import * as Iconv from "iconv-lite";
-import * as fs from "fs";
+import { readFileSync, writeFile } from "fs";
+// import * as Iconv from "iconv-lite";
+import * as Iconv from "iconv";
 
 export class Model {
   private static editor: vscode.TextEditor;
+  private static readonly iconv = new Iconv('UTF-8', 'ISO-2022-JP');
 
   constructor(editor: vscode.TextEditor) {
     Model.editor = editor;
@@ -15,10 +17,8 @@ export class Model {
     return Model.editor?.document.getText();
   }
 
-  private static get convertToUTF8() {
-    const buf = Buffer.from(Model.fileDoc, 'binary');
-    const docUtf8 = Iconv.decode(buf, 'utf-8');
-    return docUtf8;
+  private static get readFile() {
+    return readFileSync(`${Model.editor?.document.uri.fsPath}`);
   }
 
   private static get unicodeArray() {
@@ -29,38 +29,20 @@ export class Model {
     return unicodeArray;
   }
 
-  private static get readFile() {
-    return fs.readFileSync(`${Model.editor?.document.uri.fsPath}`);
-  }
-
   get charCode() {
-    return Jschardet.detect(Model.fileDoc).encoding;
+    return Jschardet.detect(Model.readFile).encoding;
   }
 
   encodeFile() {
-    const Utf8Array = encoding.convert(Model.unicodeArray, {
+    const Utf8Array = encoding.convert(Model.readFile, {
       to: 'UTF8',
       from: 'AUTO'
     });
-    const text = encoding.codeToString(Utf8Array);
+    // const text = encoding.codeToString(Utf8Array);
+    const text = Model.iconv.convert(Model.readFile);
 
-    vscode.window.showInformationMessage(`${Model.readFile}`)
-
-    const startPos = new vscode.Position(0, 0);
-    const endPos = new vscode.Position(Model.editor?.document.lineCount - 1, 10000);
-    const curSelection = new vscode.Selection(startPos, endPos);
-    const callback = function (editBuilder: vscode.TextEditorEdit): void {
-      editBuilder.replace(curSelection,text);
-    }
-
-    Model.editor.edit(callback);
-  }
-}
-
-function assertIsDefined<T>(val: T): asserts val is NonNullable<T> {
-  if (val === undefined || val === null) {
-    throw new Error(
-      `Expected 'val' to be defined, but received ${val}`
-    );
+    writeFile(`${Model.editor?.document.uri.fsPath}`, text, (err) => {
+      if (err) vscode.window.showInformationMessage(`${err}`);
+    })
   }
 }
